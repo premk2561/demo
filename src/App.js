22b1a4ad8820b2da4e4379a4217be2b685a020d2
prem <premkgundala@gmail.com>
2021-10-14 09:35:21 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Prem Kumar ...!</h1>
+      <p>its working fine now</p>
+    </div>
+  );
+}
+
+export default App;
